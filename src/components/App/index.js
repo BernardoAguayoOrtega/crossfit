fd23a42ece415746e34fd83c43b485796f99d526
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-08-30 20:10:27 -0500

    finish set up

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+//import react
+import React from 'react'
+//import material design
+import { Typography } from '@material-ui/core'
+
+//create functional component and export it
+export const App = () => {
+  return(
+    <>
+      <Typography variant='h3' color="primary">I'm the app!!</Typography>
+    </>
+  )
+}
---
+++ src/index.js
@@ -0,0 +1,19 @@
+//import react
+import React from 'react';
+//import render method
+import { render } from 'react-dom';
+//import hash router component
+import { HashRouter as Router } from 'react-router-dom';
+//import app component
+import { App } from './components/App';
+
+//create root const
+const root = document.getElementById('root');
+
+//render the app
+render(
+	<Router basename='/'>
+		<App />
+	</Router>,
+	root,
+);
