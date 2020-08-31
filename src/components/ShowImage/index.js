c05d908d4a68a910c91a5ef377b98d9eea223a21
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-08-30 21:27:43 -0500

    show image succed

--- src/components/ShowImage/index.js
+++ src/components/ShowImage/index.js
@@ -4,14 +4,21 @@ import React from 'react';
 import { Typography } from '@material-ui/core';
 //import image container
 import { ImageContainer } from './styles';
+//import title
+import { Title } from './styles'
 
 //create and export Show Image component
 export const ShowImage = () => {
 	return (
 		<ImageContainer>
-			<Typography align='center' color='primary' variant='h3'>
-				What are you waiting for?
-			</Typography>
+			<Title>
+				<Typography 
+				align='center' 
+				color='primary' 
+				variant='subtitle1'>
+					What are you waiting for?
+				</Typography>
+			</Title>
 		</ImageContainer>
 	);
 };
--- src/components/ShowImage/styles.js
+++ src/components/ShowImage/styles.js
@@ -2,10 +2,18 @@
 import styled from 'styled-components';
 
 //image const
-const image = '../../images/image4.jpg';
+const image = 'https://images.unsplash.com/photo-1580086320409-b5f0cb225046';
 
 //create and export ImageContainer
 export const ImageContainer = styled.div`
-	background-size: contain;
+	background-position: center;
+	background-repeat: no-repeat;
+	background-size: cover;
 	background-image: url(${image});
+  height: 80vh;
 `;
+
+//create and export title
+export const Title = styled.div`
+  background: black;
+`
